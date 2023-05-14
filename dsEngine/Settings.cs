be8d93b87f15afad186fdbdcf79367f3888772f9
Gavin St. George <62919189+gstgeorge@users.noreply.github.com>
2023-05-14 16:44:00 -0500

    init settings class

---
+++ dsEngine/Settings.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+
+namespace dsEngine
+{
+    internal class Settings
+    {
+        public static string SETTINGS_PATH
+        {
+            get
+            {
+                string path = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar
+                    + "data" + Path.DirectorySeparatorChar;
+
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                return path;
+            }
+        }
+
+        public static string DEALER_SETTINGS_PATH
+        {
+            get
+            {
+                string path = SETTINGS_PATH + "dealers" + Path.DirectorySeparatorChar;
+
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                return path;
+            }
+        }
+    }
+}
