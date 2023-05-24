5e55717dd3a124bc7ae280fae2e83bbe50966211
Gavin St. George <62919189+gstgeorge@users.noreply.github.com>
2023-05-23 19:22:26 -0500

    create WorkOrder class

---
+++ dsEngine/WorkOrder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dsEngine
+{
+    internal class WorkOrder
+    {
+        public SortedSet<Vehicle> Vehicles { get; } = new SortedSet<Vehicle>();
+    }
+}
