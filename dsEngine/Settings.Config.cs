7914c9b96dad032b5b705a32987e2c583a15e27f
Gavin St. George <62919189+gstgeorge@users.noreply.github.com>
2023-05-25 15:25:19 -0500

    add user config

---
+++ dsEngine/Settings.Config.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dsEngine
+{
+    internal partial class Settings
+    {
+        internal class Config
+        {
+            private string _logoFilename;
+
+            public string LogoFilename 
+            {
+                get => _logoFilename;
+                set 
+                {
+                    _logoFilename = (value ==  null) ? null : Settings.LOGO_DIR + value;
+                } 
+            }
+        }
+    }
+}
--- dsEngine/Settings.cs
+++ dsEngine/Settings.cs
@@ -1,38 +1,58 @@
-﻿using System.IO;
+﻿using Newtonsoft.Json;
+using System.IO;
 
 namespace dsEngine
 {
-    internal class Settings
+    internal partial class Settings
     {
-        public static string SETTINGS_PATH
+        public static string SETTINGS_DIR
         {
             get
             {
-                string path = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar
-                    + "data" + Path.DirectorySeparatorChar;
-
-                if (!Directory.Exists(path))
-                {
-                    Directory.CreateDirectory(path);
-                }
-
-                return path;
+                return EnsurePathExists(Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar
+                    + "data" + Path.DirectorySeparatorChar);
             }
         }
 
-        public static string DEALER_SETTINGS_PATH
+        public static string DEALER_SETTINGS_DIR
         {
             get
             {
-                string path = SETTINGS_PATH + "dealers" + Path.DirectorySeparatorChar;
-
-                if (!Directory.Exists(path))
-                {
-                    Directory.CreateDirectory(path);
-                }
-
-                return path;
+                return EnsurePathExists(SETTINGS_DIR + "dealers" + Path.DirectorySeparatorChar);
             }
+        }
+
+        public static Config UserConfig { get; private set; } = new Config();
+
+        private static string LOGO_DIR
+        {
+            get
+            {
+                return EnsurePathExists(SETTINGS_DIR + "logo" + Path.DirectorySeparatorChar);
+            }
+        }
+
+        public static void SaveUserConfig()
+        {
+            File.WriteAllText("config.json", JsonConvert.SerializeObject(UserConfig));
+        }
+
+        public static void LoadUserConfig()
+        {
+            if (File.Exists(SETTINGS_DIR + "config.json"))
+            {
+                UserConfig = JsonConvert.DeserializeObject<Config>(File.ReadAllText("config.json"));
+            }
+        }
+
+        private static string EnsurePathExists(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            return path;
         }
     }
 }
